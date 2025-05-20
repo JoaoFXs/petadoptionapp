ed4fa2d4636f3e017574c9ca5c838d00b1f42078
jovibfel@gmail.com <jovibfel@gmail.com>
2025-05-19 21:27:48 -0300

    Create authentication.service

--- src/resources/user/authentication.service.ts
+++ src/resources/user/authentication.service.ts
@@ -1,1 +1,30 @@
-import { AccessToken, Credentials, User, UserSessionToken } from './user.resource'}
+import { AccessToken, Credentials, User, UserSessionToken } from './user.resource';
+import jwt from 'jwt-decode';
+
+
+
+
+class AuthService{
+
+
+    baseURL: string = process.env.NEXT_PUBLIC_API_URL + '/v1/users';
+    // Static variable used to store the session token in the browser's localStorage
+    static AUTH_PARAM: string = '_auth';
+    
+    // Method to authenticate a user with given credentials (email and password)
+    async authenticate(credentials: Credentials): Promise<AccessToken>{
+        console.log(credentials);
+        const response = await fetch(this.baseURL + '/auth', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(credentials),  
+        });
+        if (response.status == 401) {
+            throw new Error('User or Password are incorrect or not registered');
+        }
+        return await response.json();
+    }
+
+
