2ba07e0388f9ea26c568ed7eecab7fd5dd4be8c1
jovibfel@gmail.com <jovibfel@gmail.com>
2025-06-25 14:37:45 -0300

    create searchByID to rescue value pet

--- src/app/available/petinfo/[petId]/page.tsx
+++ src/app/available/petinfo/[petId]/page.tsx
@@ -1,25 +1,53 @@
 'use client'
 
 import { Template } from "@/components";
-import { use } from "react";
-export interface PetInfoProps{
-   params: Promise<{
+import { usePetService } from '@/resources/pet/pet.service';
+import React, { useState, useEffect } from 'react';
+import { Pet } from "@/resources";
+
+export interface PetInfoProps {
+  params: Promise<{
     petId: string;
   }>;
 }
 
+const PetInfo: React.FC<PetInfoProps> = ({ params }) => {
+  const usePet = usePetService();
+  const [petId, setPetId] = useState<string>('');
+  const [pet, setPet] = useState<Pet>();
 
-const PetInfo: React.FC<PetInfoProps> = ({ params }: PetInfoProps) => {
-  const { petId } = use(params);
-    return(
-        <Template>
-          <div className="p-6">
-        <h1 className="text-3xl font-bold text-green-700">Pet Info: {petId}</h1>
+  useEffect(() => {
+    const fetchParams = async () => {
+      const resolved = await params;
+       console.log("Pet ID:", resolved.petId); // log útil
+      setPetId(resolved.petId);
+    };
+
+    fetchParams();
+  }, [params]);
+
+  useEffect(() => {
+    const fetchPet = async () => {
+      if (petId) {
+        const result = await usePet.searchByID(petId.trim());
+          console.log("Resultado da busca:", result); // Adicione aqui
+    
+        setPet(result); 
+      }
+    };
+
+    fetchPet();
+  }, [petId]);
+
+  return (
+    <Template>
+      <div className="p-6">
+        <h1 className="text-3xl font-bold text-green-700">
+          Pet Info: {pet?.name}
+        </h1>
       </div>
-        </Template>
-
-    )
-
+    </Template>
+  );
 };
 
 export default PetInfo;
--- src/resources/pet/pet.service.ts
+++ src/resources/pet/pet.service.ts
@@ -20,11 +20,28 @@ async search(query: string = "", available?: boolean | null ): Promise<Pet[]> {
 
   // Junta os parâmetros, se houver
   url += params.join("&");
-
+  console.log("URL UTILIZADA" + url)
   const response = await fetch(url);
   return await response.json();
 }
 
+async searchByID(query: string = ""): Promise<Pet> {
+  let url = `${this.baseURL}/search?`;
+
+  const params: string[] = [];
+
+  if (query.trim() !== "") {
+    params.push(`id=${encodeURIComponent(query)}`);
+  }
+
+  // Junta os parâmetros, se houver
+  url += params.join("&");
+  console.log("URL UTILIZADA" + url)
+  const response = await fetch(url);
+  return await response.json();
 }
+}
+
+
 
 export const usePetService = () => new PetService(); 
